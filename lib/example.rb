5d6e6be7f0de35ce04a436eb0e26589aba397a80
Phil Newman <pnewman@viimed.com>
2019-11-10 22:13:53 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,8 +1,9 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
+def say_hello_world
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
 puts phrase
 puts phrase
 puts phrase
